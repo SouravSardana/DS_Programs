6870de7dea9c6a2960af39e80789fdbaa8c3c29e
TaniaAggarwal29 <goyaltania64@gmail.com>
2021-10-01 17:47:52 +0530

    added program on checking parenthesis

---
+++ CheckParenthesis/cmake-build-debug/CMakeFiles/CheckParenthesis.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CheckParenthesis.dir/main.cpp.obj"
+  "CheckParenthesis.exe"
+  "CheckParenthesis.exe.manifest"
+  "CheckParenthesis.pdb"
+  "libCheckParenthesis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CheckParenthesis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
